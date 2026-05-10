d5d0487e125cdbccf97a9206e3326447680ba32a
url-p <danekapplinger@gmail.com>
2026-05-09 23:30:12 -0500

    added compatibilty for ui
    added more events to interacting pipeline (interact start vs finish)
    improved interactables template
    changed dispatching to be controlled by InGameNightMain

--- wild-jam-93/OURSTUFF/Night/in_game_night_main.gd
+++ wild-jam-93/OURSTUFF/Night/in_game_night_main.gd
@@ -2,13 +2,15 @@ extends Node2D
 #this script is in charge of everything involved during the night section of the game
  
 var Camp #camp scene
-var stageCoach = preload("res://OURSTUFF/Night/dev_stagecoach.tscn") #stage coach scene
-var star = preload("res://OURSTUFF/Night/stagecoachInteractables/star.tscn") #star scene
+const stageCoach = preload("res://OURSTUFF/Night/dev_stagecoach.tscn") #stage coach scene
+const star = preload("res://OURSTUFF/Night/stagecoachInteractables/star.tscn") #star scene
 
-@export var stageCoachSelectionRange := 50.0
+@export var selectionRange := 50.0
 
 var stageCoaches = [] #array of all stagecoaches
+var interactables = [] #array of stagecoach interactables
 var selectedStageCoach
+var selectedInteractable
 
 var starSpawnTimer
 
@@ -31,10 +33,16 @@ func _process(delta: float) -> void:
 		pass
 		if selectedStageCoach == null:
 			selectStageCoach()
-		else:
-			dispatchStageCoach()
+		if selectedInteractable == null:
+			selectInteractable()
+		
 	elif Input.is_action_just_pressed("mouseSecondary"):
 		selectedStageCoach = null
+		selectedInteractable = null
+		# can also be deselected via ui
+	
+	if Input.is_action_just_pressed("dev1"):
+		dispatchStagecoach()
 
 #func spawnStar(): #spawn star OLD
 #	var temp = star.instantiate()
@@ -43,20 +51,39 @@ func _process(delta: float) -> void:
 
 func selectStageCoach():
 	pass #attempt to select coach at mouse position, if one exists
+	#right now you can select a moving coach and divert it, but that cna change
 	for i in stageCoaches.size():
 		var distance = (get_global_mouse_position() - stageCoaches[i].global_position).length()
-		if distance <= stageCoachSelectionRange:
+		if distance <= selectionRange:
 			selectedStageCoach = stageCoaches[i]
-			print(" selected")
+			#highlight coach or something
+			print("selected")
 			break
-		else: 
-			print(i, " not in range")
 
-func dispatchStageCoach():
-	pass #send selected coach to mouse position
-	selectedStageCoach.dispatch(get_global_mouse_position())
-	selectedStageCoach = null
-	print(" dispatched")
+func selectInteractable():
+	pass #attempt to select anyinteractable
+	for i in interactables.size():
+		if interactables[i] != null: #jank. check in any null values in array and delete
+			var distance = (get_global_mouse_position() - interactables[i].global_position).length()
+			if distance <= selectionRange:
+				selectedInteractable = interactables[i]
+				#call a function to spawn ui element here
+				print("selected")
+				break
+		else:
+			interactables.remove_at(i)
+
+
+
+func dispatchStagecoach(): # called by the ui when player clicks dispatch
+	if (selectedInteractable != null) && (selectedStageCoach != null):
+		if selectedInteractable.canInteract(selectedStageCoach) == true:
+			interactables.erase(selectedInteractable)
+			selectedStageCoach.dispatch(selectedInteractable)
+			selectedInteractable = null
+			selectedStageCoach = null
+			print("dispatched")
+
 
 
 func _on_star_spawn_timer_timeout() -> void:
@@ -69,4 +96,9 @@ func spawnInteractable(type: GlobalEnums.spawnables, location: Vector2): #contro
 		GlobalEnums.spawnables.STAR:
 			var temp =  star.instantiate()
 			temp.global_position = location
+			interactables.append(temp)
 			add_child(temp)
+
+func deleteInteractable(node: Node2D):
+	interactables.erase(node)
+	node.queue_free()
