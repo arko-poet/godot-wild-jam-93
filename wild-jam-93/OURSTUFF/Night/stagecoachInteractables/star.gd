5dfc99a70177cdddd25b8b36fb54c77dc3d0c196
arko <arkadiusz.kupracz@gmail.com>
2026-05-17 20:14:59 +0100

    can remember whats here

--- wild-jam-93/OURSTUFF/Night/stagecoachInteractables/star.gd
+++ wild-jam-93/OURSTUFF/Night/stagecoachInteractables/star.gd
@@ -58,9 +58,9 @@ func _draw() -> void:
 		draw_circle(
 				Vector2(0, 0),
 				max(collision_shape_2d.shape.size.x, collision_shape_2d.shape.size.y) / 2,
-				Color("#A855F7"),
+				Color("a855f778"),
 				false,
-				4.0,
+				16.0,
 		)
 
 
