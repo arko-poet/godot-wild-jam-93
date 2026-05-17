5f9258d82ed24261f67539224be08e47c223ad6c
url-p <danekapplinger@gmail.com>
2026-05-17 13:00:28 -0500

    added voicelines for powerups

--- wild-jam-93/OURSTUFF/Night/power_ups/max_stamina.gd
+++ wild-jam-93/OURSTUFF/Night/power_ups/max_stamina.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var inGameNightMain
 @onready var timer: Timer = $Timer
+@onready var sfx: AudioStreamPlayer = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 	inGameNightMain = find_parent("InGameNightMain")
 	timer.start(.1)
+	sfx.play()
 	for i in inGameNightMain.stageCoaches:
 		i.stamina = Camp.MAX_STAMINA
 
